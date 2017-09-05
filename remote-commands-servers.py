dd463446118a1d248df00dc8719da0693076295d
dennyzhang <denny@dennyzhang.com>
2017-09-05 18:45:59 -0500

    add scripts

---
+++ remote-commands-servers.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+##-------------------------------------------------------------------
+## @copyright 2017 DennyZhang.com
+## Licensed under MIT
+##   https://raw.githubusercontent.com/DennyZhang/devops_public/master/LICENSE
+##
+## File : remote-commands-servers.py
+## Author : Denny <contact@dennyzhang.com>
+## Description :
+## --
+## Created : <2017-09-05>
+## Updated: Time-stamp: <2017-09-05 18:45:09>
+##-------------------------------------------------------------------
+def test():
+    print "hello, world"
+    
+if __name__ == '__main__':
+    test()
+## File : remote-commands-servers.py ends
