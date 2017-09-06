5417364301cbff9c7a0c59a2171125030a96ff61
dennyzhang <denny@dennyzhang.com>
2017-09-05 19:13:27 -0500

    add python doc

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Travis CI Status: [![Build Status](https://travis-ci.org/DennyZhang/remote-comma
 Run remote ssh commands on multiple servers
 
 People can:
-1. People can choose how many concurrent executors we support.
+1. People can choose how many concurrent executors we support. By default, it's 1.
 2. If the count of concurrent executors is 1, we run remote ssh commands sequentially.
 3. People can specify whether to quit and abort following servers, if previous batch has any errors.
 
--- remote-commands-servers.py
+++ remote-commands-servers.py
@@ -9,11 +9,41 @@
 ## Description :
 ## --
 ## Created : <2017-09-05>
-## Updated: Time-stamp: <2017-09-05 19:05:41>
+## Updated: Time-stamp: <2017-09-05 19:13:17>
 ##-------------------------------------------------------------------
-def test():
-    print("hello, world")
+import sys
+import paramiko
+import argparse
+
+def run_remote_ssh(server, username, ssh_port, ssh_key_file, key_passphrase, ssh_command):
+    import logging
+    logging.getLogger("paramiko").setLevel(logging.WARNING)
+    output = ""
+    info_dict = {}
+    try:
+        ssh = paramiko.SSHClient()
+        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        key = paramiko.RSAKey.from_private_key_file(ssh_key_file, password=key_passphrase)
+        ssh.connect(server, username=username, port=ssh_port, pkey=key)
+        stdin, stdout, stderr = ssh.exec_command(ssh_command)
+        output = "\n".join(stdout.readlines())
+        ssh.close()
+        info_dict = json.loads(output)
+        return (server, info_dict, None)
+    except:
+        return (server, {}, "Unexpected on server: %s error: %s" % (server, sys.exc_info()[0]))
 
 if __name__ == '__main__':
-    test()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--server_list', required=True, \
+                        help="A list of servers to run the remote ssh commands", type=str)
+    parser.add_argument('--executor_count', default=1, \
+                        help="How many concurrent executors to run. Default value is 1.", type=int)
+    parser.add_argument('--ssh_username', default="root", \
+                        help="SSH username", type=str)
+    parser.add_argument('--ssh_key_file', required=True, \
+                        help="SSH private key file. Here we assume the same key file works for all servers.", type=str)
+    parser.add_argument('--key_passphrase', default="", \
+                        help="Key passphrase for SSH private key file. If not given, key file is assumed unencrypted.", \
+                        type=str)
 ## File : remote-commands-servers.py ends
